c5c98be7e6a6cdce554942e3e2f6269695588a85
danim <daniel.marin@alumnos.upm.es>
2023-01-22 17:35:04 +0100

    minor

--- GUI.py
+++ GUI.py
@@ -73,13 +73,13 @@ def open():
 def predecir():
 
     
-    tf.keras.backend.clear_session()  # Para restablecer fácilmente el estado del portátil. No necesario o si ni idea jiji
+    #tf.keras.backend.clear_session()  # Para restablecer fácilmente el estado del portátil. No necesario o si ni idea jiji
 
 
     current_directory = os.path.dirname(os.path.realpath(__file__))
     model = tf.keras.models.load_model((current_directory + "\Model\model.h5"),  custom_objects={'KerasLayer':hub.KerasLayer})
 
-    model.summary()
+    #model.summary()
 
 
     image = PIL.Image.open(root.filename)
@@ -108,7 +108,7 @@ def predecir():
     predicted_id = tf.math.argmax(predicted_batch, axis=-1)
     predicted_id = predicted_id.numpy() #Sepasa a numpy porque el formato es raro (tuple)
 
-    print(predicted_id) #Número correspondiente al personaje predicho
+    #print(predicted_id) #Número correspondiente al personaje predicho
 
     #Esta es la lista para convertir el número predicho en su correspondiente label
     class_names = ["Alfredo Linguini de Ratatoui","Boo de Monstruos S.A","Capitan B. McCrea de Wall-E","Carl Fredricksen de Up","Charles Muntz de Up","Chick Hicks de cars","Dash de los increibles","Doc Huston de cars","Dori de Buscando a Nemo","Edna de los increibles","Elastic girl de los increibles","Eva de Wall-E","Flo de cars","Frozono de los increibles","Gill de Buscando a Nemo","Guido de cars","Henry J. Waternoose III de Monstruos S.A","Jessie de Toy Story 3","Lightyear de Toy Story 3","Lotso de Toy Story 3","Luigi de cars","Marlin de Buscando a Nemo","Mate de cars","Mike Wazowski de Monstruos S.A","Mr. Increible de los increibles","Ramon de cars","Rayo de cars","Remy de Ratatoui","Rex de Toy Story 3","Russell de Up","Sally de cars","Sindrome de los increibles","Skinner de Ratatoui","Sr. Potato de Toy Story 3","Strip 'The King' Weathers de cars","Sully de Monstruos S.A","Violeta de los increibles","Wall-E de Wall-E","Woody de Toy Story 3","Nemo de Buscando a Nemo","Randall Boggs de Monstruos S.A"]
