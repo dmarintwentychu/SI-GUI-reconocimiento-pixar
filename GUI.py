092befd52fe19a42d9e625f7d6af3bd0bcfed12d
br0060 <a.rromero@alumnos.upm.es>
2023-01-22 14:50:29 +0100

    commit 3

--- GUI.py
+++ GUI.py
@@ -113,8 +113,9 @@ def predecir():
     #Esta es la lista para convertir el número predicho en su correspondiente label
     class_names = ["Alfredo Linguini de Ratatoui","Boo de Monstruos S.A","Capitan B. McCrea de Wall-E","Carl Fredricksen de Up","Charles Muntz de Up","Chick Hicks de cars","Dash de los increibles","Doc Huston de cars","Dori de Buscando a Nemo","Edna de los increibles","Elastic girl de los increibles","Eva de Wall-E","Flo de cars","Frozono de los increibles","Gill de Buscando a Nemo","Guido de cars","Henry J. Waternoose III de Monstruos S.A","Jessie de Toy Story 3","Lightyear de Toy Story 3","Lotso de Toy Story 3","Luigi de cars","Marlin de Buscando a Nemo","Mate de cars","Mike Wazowski de Monstruos S.A","Mr. Increible de los increibles","Ramon de cars","Rayo de cars","Remy de Ratatoui","Rex de Toy Story 3","Russell de Up","Sally de cars","Sindrome de los increibles","Skinner de Ratatoui","Sr. Potato de Toy Story 3","Strip 'The King' Weathers de cars","Sully de Monstruos S.A","Violeta de los increibles","Wall-E de Wall-E","Woody de Toy Story 3","Nemo de Buscando a Nemo","Randall Boggs de Monstruos S.A"]
 
-    print(class_names[predicted_id[0]]) # Este es el valor que predice
-    lamda : ventanaFinal(class_names[predicted_id[0]])
+    #print(class_names[predicted_id[0]]) # Este es el valor que predice
+    #Llamamos a la ventana final que pone el personaje y la predicción
+    ventanaFinal(class_names[predicted_id[0]])
 
 
 #Pregunta de si quiere predecir
@@ -129,6 +130,7 @@ def confirmacion():
 #Ventana mostrando el pj y preguntando si ha acertado
 def ventanaFinal(nombrePj):
     global imagenPersonaje
+    print(nombrePj)
 
     
 
